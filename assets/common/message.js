d62e8cb5f332831683b7d66abdec5bdcf5efc841
balaitbalagithubrit <c.l.bala20@gmail.com>
2024-07-08 18:14:43 +0530

    #final changes of the site

--- assets/common/message.js
+++ assets/common/message.js
@@ -1,31 +1,43 @@
 function submitForm() {
+  if (!validateForm()) return false;
   var name = document.getElementById("name").value.trim();
   var email = document.getElementById("email").value.trim();
   var message = document.getElementById("message").value.trim();
+  sendMessage(name, email, message);
 
-  if (!validateForm()) return false;
-
-  // Show "Submitting..." message
-  document.getElementById("submittingMsg").style.display = "inline";
-
-  setTimeout(function () {
-    // Simulated response received, you can replace this with actual logic
-    alert("Form submitted successfully!");
-    document.getElementById("myForm").reset(); // Reset form after successful submission
-    document.getElementById("submittingMsg").style.display = "none"; // Hide "Submitting..." message
-  }, 2000); // Simulated 2 second delay
-
-  return false; // Prevent actual form submission for demonstration purposes
+  return false;
 }
 
-// Function to validate form fields
+async function sendMessage(name, email, message) {
+  const submittingMsg = document.getElementById("submittingMsg");
+  const successResponse = document.getElementById("successResponse");
+  submittingMsg.textContent = "Sending...";
+  const response = await fetch(
+    "https://qhfspffzlc.execute-api.ap-south-1.amazonaws.com/send-mail",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, message }),
+    }
+  );
+  document.getElementById("myForm").reset()
+  if (!response.ok || response?.status == 500) {
+    submittingMsg.textContent = "Something Went Wrong.";
+  } else if (response.status == 200) {
+    successResponse.textContent =
+      "Thank you! Your message has been successfully received. We will get back to you shortly.";
+    submittingMsg.textContent = "";
+  }
+}
+
 function validateForm() {
   var name = document.getElementById("name").value.trim();
   var email = document.getElementById("email").value.trim();
   var message = document.getElementById("message").value.trim();
   var isValid = true;
 
-  // Validate Name
   if (name === "") {
     document.getElementById("nameError").textContent = "Name is required";
     isValid = false;
@@ -33,12 +45,10 @@ function validateForm() {
     document.getElementById("nameError").textContent = "";
   }
 
-  // Validate Email
   if (email === "") {
     document.getElementById("emailError").textContent = "Email is required";
     isValid = false;
   } else {
-    // Basic email format validation
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
       document.getElementById("emailError").textContent =
@@ -49,7 +59,6 @@ function validateForm() {
     }
   }
 
-  // Validate Message
   if (message === "") {
     document.getElementById("messageError").textContent = "Message is required";
     isValid = false;
@@ -60,7 +69,6 @@ function validateForm() {
   return isValid;
 }
 
-// Function to clear error message when user starts typing in a field
 document.getElementById("name").addEventListener("input", function () {
   document.getElementById("nameError").textContent = "";
 });
@@ -78,6 +86,8 @@ document.getElementById("message-icon").addEventListener("click", function () {
 
   if (messageContainer.style.display === "none") {
     messageContainer.style.display = "block";
+    document.getElementById("submittingMsg").textContent = "";
+    document.getElementById("successResponse").textContent = "";
     document.getElementById("messageError").textContent = "";
     document.getElementById("nameError").textContent = "";
     document.getElementById("emailError").textContent = "";
